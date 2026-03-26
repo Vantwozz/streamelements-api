596b18d6de6084a0faac024eee81ab7e0a063bdd
Kapitanov Ivan <95244485+Vantwozz@users.noreply.github.com>
2026-03-26 16:29:07 +0300

    Update pts.js

--- api/pts.js
+++ api/pts.js
@@ -1,17 +1,14 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   const chance = Math.random();
   let result;
 
-  // 75% шанс на отрицательное число
   if (chance < 0.75) {
-    // от -65 до -1
-    result = -Math.floor(Math.random() * 65) - 1;
+    result = -Math.floor(Math.random() * 65) - 1; // -65 до -1
   } else {
-    // от 0 до 400
-    result = Math.floor(Math.random() * 401);
+    result = Math.floor(Math.random() * 401); // 0 до 400
   }
 
   res.status(200).json({
     message: `Rolling points for the next game... 🎲 The next game will be worth ${result} pts!`
   });
-};
+}
